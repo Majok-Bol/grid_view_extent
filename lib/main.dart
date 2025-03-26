0c56774b4fed371d89358493f0f34afc6829cfb5
Majok-Bol <bolmajok001@gmail.com>
2025-03-26 17:45:33 +0300

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,40 @@ void main(){
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
+  final List<String>ImagePath=[
+    "assets/images/p1.jpg",
+    "assets/images/p2.jpg",
+    "assets/images/p3.jpeg",
+    "assets/images/p4.jpeg",
+    "assets/images/p5.jpeg",
+    "assets/images/p6.jpeg",
+    "assets/images/p7.jpeg",
+    "assets/images/p8.jpeg",
+    "assets/images/profile1.webp",
+    "assets/images/profile2.webp",
+    "assets/images/profile3.webp",
+    "assets/images/profile4.webp"
+  ];
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('GridView.extent widget',style: TextStyle(color: Colors.white,fontSize: 20),),centerTitle: true,backgroundColor: Colors.purple,),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.all(10),
-          color: Colors.blueGrey,
-        ),
-
+        body: Expanded(
+          // padding: EdgeInsets.all(10),
+          // margin: EdgeInsets.all(10),
+          // color: Colors.blueGrey,
+          child: Column(children: [
+            GridView.extent(maxCrossAxisExtent:500,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+      children: ImagePath.map((path) => Image.asset(path)).toList(),
+          ),
+          ],
       ),
+    ),
+    ),
     );
   }
 }
